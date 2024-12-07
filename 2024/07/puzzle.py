bee014c9bc7fbcc7d6938f67f1191cd86703b505
Flo Gehring <florian.gehring@pm.me>
2024-12-07 10:56:56 +0100

    07_02_cleanup

--- 2024/07/puzzle.py
+++ 2024/07/puzzle.py
@@ -1,5 +1,5 @@
 from typing import List
-from itertools import permutations, combinations_with_replacement, product
+from itertools import  product
 equations = [line.strip() for line in open("2024/07/input.txt")]
 equations = [line.split(":") for line in equations]
 equations = [(int(eq[0]), [ int(op.strip()) for op in eq[1].split()]) for eq in equations]
@@ -17,15 +17,17 @@ def test_op_sequence(test_value: int, operands: List[int], operators: List[str])
             raise Exception("Unknown Op: " + op )
     return test_value == result
 
-def testEquations(test_value: int, operands: List[int]) -> bool:
-    operators = ["+", "*", "||"]
+def test_equations(test_value: int, operands: List[int], operators=["+", "*", "||"]) -> bool:
     op_sequences = list(product(operators, repeat=len(operands)-1)) 
     
     return any([test_op_sequence(test_value, operands, ops) for ops in op_sequences])
 
+# Part 1
+true_eqs_part1= [eq for eq in equations if test_equations(eq[0], eq[1], operators=["+" ,"*" ])]
+true_results_part1 = [eq[0] for eq in true_eqs_part1]
+print("Result Part 1: ", sum(true_results_part1))
 
-true_eqs= [eq for eq in equations if testEquations(eq[0], eq[1])]
-print(true_eqs)
-true_results = [eq[0] for eq in true_eqs]
-print(true_results)
-print(sum(true_results))
+# Part 2
+true_eqs_part2= [eq for eq in equations if test_equations(eq[0], eq[1], operators=["+" ,"*", "||" ])]
+true_results_part2 = [eq[0] for eq in true_eqs_part2]
+print("Result Part 2: ",  sum(true_results_part2))
