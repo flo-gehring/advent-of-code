26d4af4bb56dabd13a9c634e3641d70fb6a9cb73
Flo Gehring <florian.gehring@pm.me>
2024-12-08 11:34:04 +0100

    07_01

--- 2024/08/puzzle.py
+++ 2024/08/puzzle.py
@@ -1,5 +1,5 @@
 from itertools import combinations
-input : list[str]= [l.strip() for l in open("2024/08/input_test.txt").readlines()]
+input : list[str]= [l.strip() for l in open("2024/08/input.txt").readlines()]
 def get_antenna_types(grid: list[str]) -> set[str]:
     result = set()
     for row in grid:
@@ -17,7 +17,9 @@ def get_antenna_locations(antenna_type: str, grid: list[str]) -> list[tuple[int,
 def calculate_antinodes(antenna_locations: list[tuple[int,int]]) -> list[tuple[int,int]]:
     antenna_combinations = combinations(antenna_locations, r=2)
     result = []
+
     for (antenna1, antenna2) in antenna_combinations:
+        print(antenna1, antenna2)
         result += calculate_antinodes_for_antennas(antenna1, antenna2)
     return result
 
@@ -37,13 +39,21 @@ def part_1(grid: list[str]):
     antinode_locations = set()
     antenna_types = get_antenna_types(grid)
     grid_y_size, grid_x_size = (len(grid), len(grid[0]))
+    print("Antenna types", antenna_types)
     for antenna_type in antenna_types:
-        antenna_locations_antenna = [
+        antinode_locations_antenna = [
             (y, x) for (y,x) in 
-            get_antenna_locations(antenna_type, grid)
+            calculate_antinodes(get_antenna_locations(antenna_type, grid))
             if y < grid_y_size and y >= 0 and x < grid_x_size and x >= 0
         ]
-        antinode_locations = antinode_locations.union(antenna_locations_antenna)
+        print(
+            "Antenna Locations " , antenna_type, get_antenna_locations(antenna_type, grid)
+        )
+        print(
+            "Antinode Locations " , antenna_type, antinode_locations
+        )
+
+        antinode_locations = antinode_locations.union(antinode_locations_antenna)
     return antinode_locations
 
 def mark_locations_on_grid(grid: list[str], locations: list[tuple[int,int]], marker: str) -> list[str]:
@@ -57,5 +67,7 @@ def pretty_grid(grid: list[str]) -> str:
         result += row + "\n"
     return result
 locations = part_1(input)
+print(locations)
 marked_locations = mark_locations_on_grid(input, locations, "#")
 print(pretty_grid(marked_locations))
+print("Solution Part 1: ", len(locations))
