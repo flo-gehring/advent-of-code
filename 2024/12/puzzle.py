d6d0ed2163e2959fd6dcd0f80819822b35ce5c40
Flo Gehring <florian.gehring@pm.me>
2024-12-15 11:05:15 +0100

    save

--- 2024/12/puzzle.py
+++ 2024/12/puzzle.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 path = "2024/12/input.txt"
 input = [[c for c in line.strip()] for line in open(path).readlines()]
 types_of_plants = set([c for c in open(path).read().strip() if c != "\n"])
@@ -5,6 +7,14 @@ types_of_plants = set([c for c in open(path).read().strip() if c != "\n"])
 
 def add_tuples(lhs: tuple[int,int], rhs: tuple[int,int]) -> tuple[int,int]:
     return (lhs[0] + rhs[0], lhs[1]+ rhs[1])
+
+
+
+class Direction(Enum):
+    UP=(-1,0)
+    DOWN=(1,0)
+    RIGHT=(0,1)
+    LEFT=(-1,0)
 
 
 class Region:
@@ -23,6 +33,35 @@ class Region:
             [Region.count_fence_segment(map, c, self.plant) for c in self.coordinates]
         )
         return fence_segments * len(self.coordinates)
+    
+    def calculate_price_part2(self, map:list[list[str]]):
+        fence_segments_not_done_yet = self.get_all_fence_segments( map)
+        number_faces = 0
+        while fence_segments_not_done_yet:
+            number_faces += 1
+            (segment_coord, dir) = fence_segments_not_done_yet.pop()
+            walking_directions = None
+            if dir == Direction.DOWN or dir == Direction.UP:
+                walking_directions = [Direction.LEFT, Direction.RIGHT]
+            elif dir == Direction.RIGHT or dir == Direction.LEFT:
+                walking_directions = [Direction.DOWN, Direction.UP]
+            else:
+                raise Exception("???")
+            for 
+
+            
+            
+
+
+
+    def get_all_fence_segments(self, map:list[list[str]]) -> list[tuple[tuple[int,int], Direction]]:
+        result = []
+        for coord in self.coordinates:
+            for dir in [Direction.DOWN, Direction.LEFT, Direction.RIGHT, Direction.UP]:
+                neighbor =add_tuples(coord, dir.value())
+                if not in_input(map, neighbor ) or get_from(map, neighbor) != self.plant:
+                    result.append((coord, dir))
+        return result
 
     staticmethod 
     def count_fence_segment( map: list[list[str]], coordinate: tuple[int,int], plant: str):
@@ -44,7 +83,7 @@ class Region:
 def get_from(input, tuple):
     return input[tuple[0]][tuple[1]]
 
-def in_input(input, tuple):
+def in_input(input: list[list[str]], tuple: tuple[int,int]):
     return tuple[0] >= 0 and tuple[0] < len(input) and tuple[1] >= 0 and tuple[1] < len(input[tuple[0]])
 
 def print_regions(input, regions: list[Region] ) -> str:
@@ -94,7 +133,6 @@ for type_of_plant in types_of_plants:
         for (x, cell) in enumerate(row):
             current_coord = (y,x)
             if cell == type_of_plant and current_coord not in region_map:
-                
                 region = create_region(input, current_coord)
                 regions.append(region)
                 for c in region.coordinates:
