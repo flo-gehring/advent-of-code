d1fd4ce979841db13267e44cddb6d9bdb005e327
Flo Gehring <florian.gehring@pm.me>
2025-12-04 19:11:40 +0100

    2025/04 02

--- 2025/04/puzzle.py
+++ 2025/04/puzzle.py
@@ -3,23 +3,41 @@ grid = [[cell for cell in line.strip()] for line in f.readlines()]
 
 
 accessible_rolls = 0
-for (y, row) in enumerate(grid):
-    for (x, cell) in enumerate(row):
-        neighboring_rolls = 0
-        if cell != "@":
-            continue
-        for y_offset in  range(-1,2):
-            for x_offset in range(-1,2):
-                if y_offset == 0 and x_offset == 0:
-                    continue
-                y_to_check = y + y_offset
-                x_to_check = x + x_offset
-                out_of_bounds =y_to_check < 0 or y_to_check >= len(grid) or x_to_check < 0 or x_to_check >= len(row)
-                if out_of_bounds:
+
+def get_accessible_rolls(grid):
+    accessible_rolls = []
+    for (y, row) in enumerate(grid):
+        for (x, cell) in enumerate(row):
+            neighboring_rolls = 0
+            if cell != "@":
+                continue
+            for y_offset in  range(-1,2):
+                for x_offset in range(-1,2):
+                    if y_offset == 0 and x_offset == 0:
+                        continue
+                    y_to_check = y + y_offset
+                    x_to_check = x + x_offset
+                    out_of_bounds =y_to_check < 0 or y_to_check >= len(grid) or x_to_check < 0 or x_to_check >= len(row)
+                    if out_of_bounds:
                       continue
-                if grid[y_to_check][x_to_check] == "@":
-                    neighboring_rolls += 1
-        if neighboring_rolls < 4:
-            accessible_rolls += 1
-print(accessible_rolls)
+                    if grid[y_to_check][x_to_check] == "@":
+                        neighboring_rolls += 1
+            if neighboring_rolls < 4:
+                accessible_rolls.append((y,x))
+    return accessible_rolls
+    
+print(len(get_accessible_rolls(grid)))
                 
+
+from copy import deepcopy
+
+
+total_rolls_removed = 0 
+while True:
+    removed_rolls = get_accessible_rolls(grid)
+    if not removed_rolls:
+        break
+    for (y,x) in removed_rolls:
+        grid[y][x] = "."
+    total_rolls_removed += len(removed_rolls)
+print(total_rolls_removed)
