3a579b198b5aedba07f2bd88b4460af98a588c8c
Flo Gehring <florian.gehring@pm.me>
2025-12-09 19:46:28 +0100

    2025/09 02 - plan for implementation

--- 2025/09/puzzle.py
+++ 2025/09/puzzle.py
@@ -7,18 +7,23 @@ test_input = False
 path = base_path + ("test.txt" if test_input else "input.txt")
 f  = open( path)
 
-
 tiles = [(int(l.split(",")[0]), int(l.split(",")[1])) for l in f.readlines()]
-recangles = []
-
+rectangles = []
 for tileId1 in range(1,len(tiles)):
     for tileId2 in range(tileId1 +1, len(tiles)):
         tile1 = tiles[tileId1]
         tile2 = tiles[tileId2]
-
-        # 4, 7 -> 4 - 7 = -3 
         width = abs(tile1[0] - tile2[0]) + 1
         height = abs(tile1[1] - tile2[1]) +1
-        recangles.append( width * height)
+        rectangles.append( width * height)
 
-print("Part 1", max(recangles))
+print("Part 1", max(rectangles))
+
+# Solution Part 2:
+# Connect all Red Tiles
+# Depth First Search to get inner area
+# For each row of the input grid, create a set (or set of spans) of tiles that are not part of green and red 
+# find largest rectangles, sort descending
+# iterate until one is found, that has no coordinates not in area.
+#   how to check: for each row the rectangle spans, check  if it cells overlap with the spans of "forbidden" cells
+
